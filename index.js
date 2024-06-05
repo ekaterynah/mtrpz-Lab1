d41df52bb813dbe949e1984069e9ec8f3aa52c6a
Katya Humeniuk <katerynahumeniuk12@gmail.com>
2024-06-05 19:12:13 +0300

    Added preformatted text in a parse function

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict'
+const fs = require('fs');
+const path = require('path');
 
 function parseMarkdown(markdown) {
     let html = '';
     const lines = markdown.split('\n');
     let inParagraph = false;
+    let inPreformatted = false;
 
     for (let line of lines) {
         if (line.trim() === '') {
@@ -16,5 +19,20 @@ function parseMarkdown(markdown) {
             }
             continue;
         }
+        if (line.startsWith('```')) {
+            if (inPreformatted) {
+                html += '</pre>';
+                inPreformatted = false;
+            } else {
+                html += '<pre>';
+                inPreformatted = true;
+            }
+            continue;
+        }
+
+
     }
-}
+
+
+    return html;
+    }
