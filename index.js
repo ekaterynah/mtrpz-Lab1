f7e449bf0eec65f5a297764bf9dbb32bb89ea3d8
Katya Humeniuk <katerynahumeniuk12@gmail.com>
2024-06-05 19:24:25 +0300

    Added comments to bold, italic, monospaced text in a parse function

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ function parseMarkdown(markdown) {
                 inParagraph = true;
             }
             line = line
-                .replace(/\*\*(.*?)\*\*/g, '<b>$1</b>') // Bold
-                .replace(/_(.*?)_/g, '<i>$1</i>') // Italic
-                .replace(/`(.*?)`/g, '<tt>$1</tt>'); // Monospaced
+                .replace(/\*\*(.*?)\*\*/g, '<b>$1</b>')
+                .replace(/_(.*?)_/g, '<i>$1</i>')
+                .replace(/`(.*?)`/g, '<tt>$1</tt>');
         }
 
         html += inPreformatted ? line + '\n' : line.trim() + ' ';
